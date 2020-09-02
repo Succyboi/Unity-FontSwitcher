f8dbad21661c72c98d2a560a279f24fa25a1c257
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2020-09-02 15:32:50 +0200

    Changed readme and uploaded source

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Unity-FontSwitcher
-An easy way to switch your font in the Unity Editor
+# Unity-Fontswitcher [![License](https://img.shields.io/badge/license-MIT-orange.svg?style=flat)](LICENSE) [![http://stupidplusplus.com](https://img.shields.io/badge/Contact%20me-Here-lightgrey)](http://stupidplusplus.com)
+`Comic sans FINALLY!!!`
+
+
+
+## First off...
+#### What is this?
+Unity by default supports two fonts:
+[Inter](https://fonts.google.com/specimen/Inter)
+and [Verdana](https://www.fonts.com/font/microsoft-corporation/verdana).
+
+This project attempts to finally allow users to hotswap fonts at their will.
+
+## This is what it looks like:
+![Image of the UI](thisiswhatitlookslike.png)
+
+## Examples
+#### Consolas (Default Visual Studio font)
+![Image of consolas](consolas.png)
+
+#### Fira Code (Popular open source coding font)
+![Image of firacode](firacode.png)
+
+#### Kenney Mini (Popular game asset font)
+![Image of kenney mini](kenneymini.png)
+
+#### Comic Mono (Monospaced obligatory font meme)
+![Image of comic mono](comicmono.png)
+
+
+## Credit
+This project uses some code based on [nukadelic's EditorFontSize.cs](https://gist.github.com/nukadelic/47474c7e5d4ee5909462e3b900f4cb82).
+
+Fonts included in this project:
+
+[Consolas](https://docs.microsoft.com/en-us/typography/font-list/consolas)
+
+[Fira Code](https://github.com/tonsky/FiraCode)
+
+[Kenney Mini](https://www.kenney.nl/assets/kenney-fonts)
+
+[Comic Mono](https://github.com/dtinth/comic-mono-font)
+
+## Last but not least...
+I hope you'll have fun playing with this. Peace out✌🏻!
---
+++ Stupid/FontSwitcher/FontSwitcher.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+#if UNITY_EDITOR
+public class FontSwitcher : EditorWindow
+{
+    Font font;
+
+    [MenuItem("Window/Switch Fonts")]
+    public static void Init()
+    {
+        FontSwitcher window = (FontSwitcher)EditorWindow.GetWindow(typeof(FontSwitcher), true, "Font Switcher");
+        window.Show();
+        window.maxSize = new Vector2(200, 200);
+        window.minSize = window.maxSize;
+    }
+
+    public static void Exit()
+    {
+        FontSwitcher window = (FontSwitcher)EditorWindow.GetWindow(typeof(FontSwitcher), true, "Font Switcher");
+        window.Close();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Select a new font to load:", EditorStyles.boldLabel);
+        font = (Font)EditorGUILayout.ObjectField("", font, typeof(Font), false);
+
+        if (GUILayout.Button("Apply font"))
+        {
+            ApplyFont(font);
+            Exit();
+        }
+
+        GUILayout.Label("");
+        GUILayout.Label("Example:", EditorStyles.boldLabel);
+
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+        GUIStyle style = EditorStyles.boldLabel;
+        Font previousFont = style.font;
+
+        style.font = font;
+        style.wordWrap = true;
+
+        GUILayout.Label("The quick brown fox jumps over the lazy dog.", style);
+
+        style.font = previousFont;
+
+        style = EditorStyles.label;
+        previousFont = style.font;
+
+        style.font = font;
+        style.wordWrap = true;
+
+        GUILayout.Label("The quick brown fox jumps over the lazy dog.", style);
+
+        style.font = previousFont;
+
+        EditorGUILayout.EndVertical();
+    }
+
+
+    public static void ApplyFont(Font newFont)
+    {
+        PropertyInfo[] editorStyleProperties;
+        PropertyInfo[] defaultGuiProperties;
+
+        BindingFlags flags = BindingFlags.Static | BindingFlags.Public | BindingFlags.GetProperty;
+        editorStyleProperties = typeof(EditorStyles).GetProperties(flags);
+        defaultGuiProperties = GUI.skin.GetType().GetProperties();
+
+        foreach (PropertyInfo i in editorStyleProperties)
+        {
+            if (PropertyInfoIsValid(i, null))
+            {
+                GUIStyle style = (GUIStyle)i.GetValue(null, null);
+                style.font = newFont;
+            }
+        }
+
+        foreach (PropertyInfo i in defaultGuiProperties)
+        {
+            if (PropertyInfoIsValid(i, GUI.skin))
+            {
+                GUIStyle style = (GUIStyle)i.GetValue(GUI.skin, null);
+                style.font = newFont;
+            }
+        }
+
+        foreach (GUIStyle style in GUI.skin.customStyles)
+        {
+            style.font = newFont;
+        }
+
+        RepaintAll();
+    }
+
+    static bool PropertyInfoIsValid(PropertyInfo x, object item)
+    {
+        if (string.IsNullOrEmpty(x.Name))
+        {
+            return false;
+        }
+        else if (x.PropertyType != typeof(GUIStyle))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private static void RepaintAll() { foreach (var w in Resources.FindObjectsOfTypeAll<EditorWindow>()) w.Repaint(); }
+}
+
+#endif
